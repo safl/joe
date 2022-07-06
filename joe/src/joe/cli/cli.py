b745cf728b5a22a27a1ca32fd50d497844666b02
Simon A. F. Lund <os@safl.dk>
2022-07-06 10:27:26 +0200

    workflow
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

--- joe/src/joe/cli/cli.py
+++ joe/src/joe/cli/cli.py
@@ -5,6 +5,30 @@ from joe.core.collector import load_worklets_from_packages
 from joe.core.command import Cijoe, default_output_path, env_from_file
 from joe.core.workflow import workflow_lint, workflow_run
 
+def run_workflows(args):
+    """Run a list of workflows"""
+
+    pass
+
+def run_worklets(args):
+    """Run a list of worklets"""
+
+    pass
+
+def run(args):
+    """Run stuff"""
+
+    pass
+
+def list(args):
+    """List different collectable resources"""
+
+    pass
+
+def lint(args):
+    """Lint a workflow"""
+
+    pass
 
 def parse_args():
     """Parse command-line interface."""
@@ -15,36 +39,50 @@ def parse_args():
     subparsers = parser.add_subparsers(dest="func", help="sub-command help")
 
     parsers = {}
-    parsers["workflow_run"] = subparsers.add_parser(
-        "workflow_run", help="Process workflow"
+    parsers["run"] = subparsers.add_parser(
+        "run", help="Run workflows and worklets"
     )
-    parsers["workflow_run"].set_defaults(func=workflow_run)
-    parsers["workflow_run"].add_argument(
-        "--env", help="Path to the environment definition"
+    parsers["run"].set_defaults(func=run)
+    parsers["run"].add_argument(
+        "--env",
+        help="Path to the environment definition"
     )
-    parsers["workflow_run"].add_argument("--workflow", help="Path to a workflow.yaml")
-    parsers["workflow_run"].add_argument(
-        "--output", default=default_output_path(), help="Path to test-results"
+    parsers["run"].add_argument(
+        "--output",
+        default=default_output_path(),
+        help="Path to output"
     )
 
-    parsers["workflow_lint"] = subparsers.add_parser(
-        "workflow_lint", help="Check the integrity of the given workflow"
+    group = parsers["run"].add_mutually_exclusive_group()
+    group.add_argument(
+        "--workflow",
+        nargs='*',
+        help="Path to one of more workflow files (yaml)",
     )
-    parsers["workflow_lint"].set_defaults(func=workflow_lint)
-    parsers["workflow_lint"].add_argument("--workflow", help="Path to a workflow.yaml")
+    group.add_argument(
+        "--worklets",
+        nargs='*',
+        help="Name of one or more worklets to run",
+    )
+
+    parsers["lint"] = subparsers.add_parser(
+        "lint", help="Check the integrity of the given workflow"
+    )
+    parsers["lint"].set_defaults(func=lint)
+    parsers["lint"].add_argument("--workflow", help="Path to a workflow.yaml")
 
     worklets = load_worklets_from_packages()
-    for function_name, function in worklets.items():
-        parsers[function_name] = subparsers.add_parser(
-            function_name, help=function.__doc__
-        )
-        parsers[function_name].set_defaults(func=function)
-        parsers[function_name].add_argument(
-            "--env", help="Path to the environment definition"
-        )
-        parsers[function_name].add_argument(
-            "--output", default=default_output_path(), help="Path to test-results"
-        )
+    #for function_name, function in worklets.items():
+    #    parsers[function_name] = subparsers.add_parser(
+    #        function_name, help=function.__doc__
+    #    )
+    #    parsers[function_name].set_defaults(func=function)
+    #    parsers[function_name].add_argument(
+    #        "--env", help="Path to the environment definition"
+    #    )
+    #    parsers[function_name].add_argument(
+    #        "--output", default=default_output_path(), help="Path to test-results"
+    #    )
 
     args = parser.parse_args()
     args.worklets = worklets
