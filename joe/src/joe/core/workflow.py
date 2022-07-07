cc2050b1d80ecb09b2392477e446302828ec4608
Simon A. F. Lund <os@safl.dk>
2022-07-07 14:29:19 +0200

    ...
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

--- joe/src/joe/core/workflow.py
+++ joe/src/joe/core/workflow.py
@@ -1,5 +1,6 @@
 import pprint
 from pathlib import Path
+import os
 import re
 
 import yaml
@@ -11,8 +12,20 @@ WORKFLOW_SUFFIX = "workflow"
 
 # TODO:
 # * Implement this
-def workflow_lint(args):
+def workflow_lint(yml):
     """Do integrity-check of workflow"""
+
+    if "steps" not in yml:
+        print("missing 'steps' in workflow file")
+        return False
+    for step in yml["steps"]:
+        if "run" in step:
+            continue
+        if "uses" in step:
+            continue
+
+        print("step has neither 'run' nor 'uses'")
+        return False
 
     return True
 
@@ -31,6 +44,57 @@ def paths_to_workflow_fpaths(paths):
 
     return workflow_files
 
+
+#
+# TODO
+#
+# * linting is needed to ensure the workflow is well-defined and provide proper
+#   error-messages when it is not
+#
+def workflow_from_fpath(fpath):
+    """Returns a workflow from the given 'fpath'"""
+
+    with open(fpath) as workflow_file:
+        yml = yaml.load(workflow_file, Loader=yaml.SafeLoader)
+
+    if not workflow_lint(yml):
+        print("Invalid workflow file")
+        return False
+
+    workflow = {
+        "docstring": yml["docstring"],
+        "steps": [],
+    }
+
+    count = 0
+    for entry in yml.get("steps", []):
+        count += 1
+        step = {
+            "id": "",   # file-system-safe identifier
+            "count": count,
+            "name": entry.get("name", "") if entry.get("name") else "unnamed step",
+            "run": "",
+            "uses": "",
+            "with": {},
+        }
+
+        if "uses" in entry:
+            step["uses"] = entry.get("uses")
+            step["with"] = entry.get("with", {})
+            step["type"] = "worklet"
+            step["id"] = f"{step['count']}_{step['type']}_{step['uses']}"
+        elif "run" in entry:
+            step["type"] = "run"
+            step["run"] = entry.get("run").strip().splitlines()
+            step["id"] = f"{step['count']}_{step['type']}"
+        else:
+            return None
+
+        workflow["steps"].append(step)
+
+    return workflow
+
+
 # TODO:
 # * Add use of the test-linter before attempting to run the workflow
 # * Add error-handling
@@ -42,44 +106,13 @@ def run_workflow_files(args):
     joe = Cijoe(config_from_file(args.config) if args.config else {}, args.output)
 
     for workflow_fpath in paths_to_workflow_fpaths(args.file_or_dir):
+        workflow = workflow_from_fpath(workflow_fpath)
 
-        with open(workflow_fpath) as workflow_file:
-            workflow = yaml.load(workflow_file, Loader=yaml.SafeLoader)
+        for step in workflow["steps"]:
+            print(step)
 
-        count = 0
-        for entry in workflow.get("steps", []):
-            count += 1
-            step = {
-                "count": count,
-                "name": "",
-                "name_fs": "",
-                "run": "",
-                "with": "",
-                "uses": {},
-            }
-
-            step["name"] = (
-                entry.get("name", "") if entry.get("name") else "unnamed step"
-            )
-
-            if "uses" in entry:
-                step["uses"] = entry.get("uses")
-                step["with"] = entry.get("with", {})
-                step["type"] = "worklet"
-            elif "run" in entry:
-                step["type"] = "run"
-                step["run"] = entry.get("run").strip().splitlines()
-            else:
-                print("invalid step-definition")
-                return 1
-
-            foo = step["uses"] if step["uses"] else "inline"
-
-            step["name"] = f"{step['count']}_{step['type']}_{foo}_{step['name']}"
-            step["name_fs"] = re.sub(
-                r"[\(\)\.\s/\\?%*:|\"<>\x7F\x00-\x1F]", "_", step["name"]
-            ).lower()
-            joe.set_output_ident(step["name_fs"])
+            joe.set_output_ident(step["id"])
+            os.makedirs(os.path.join(joe.output_path, step["id"]), exist_ok=True)
 
             if step["type"] == "run":
                 for cmd in step["run"]:
