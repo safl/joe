17e929f3486dd8f2385e4b2772da411b10547e5e
Simon A. F. Lund <os@safl.dk>
2022-08-05 14:49:15 +0200

    ...
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

---
+++ joe-pkg-qemu/src/joe/qemu/worklets/build_x86.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+    Builds qemu system(x86_64-softmmu), disabling most graphics related features, and
+    enabling virtfs and debugging.
+"""
+
+
+def worklet_entry(cijoe, args, step):
+    """Build qemu"""
+
+    conf = cijoe.config["qemu"]
+    build_dir = os.path.join(conf["repository"], "build")
+
+    configure_args = [
+        '--audio-drv-list=""',
+        "--disable-docs",
+        "--disable-opengl",
+        "--disable-virglrenderer",
+        "--disable-vte",
+        "--disable-gtk",
+        "--disable-sdl",
+        "--disable-spice",
+        "--disable-vnc",
+        "--disable-curses",
+        "--disable-xen",
+        "--disable-smartcard",
+        "--disable-libnfs",
+        "--disable-libusb",
+        "--disable-glusterfs",
+        "--enable-virtfs",
+        "--enable-debug",
+        "--prefix={conf['build']['prefix']}",
+        "--target-list=x86_64-softmmu",
+    ]
+
+    cijoe.run(f"mkdir -p {build_dir}")
+    cijoe.run(f"./configure", cwd=build_dir)
+    cijoe.run("make -j $(nproc)", cwd=build_dir)
