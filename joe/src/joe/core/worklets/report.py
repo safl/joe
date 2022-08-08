c221a3c687954b76775870fca2c391d430f45ce3
Simon A. F. Lund <os@safl.dk>
2022-08-08 23:11:46 +0200

    ...
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

--- joe/src/joe/core/worklets/report.py
+++ joe/src/joe/core/worklets/report.py
@@ -1,10 +1,8 @@
 from pathlib import Path
-
-import jinja2
 import yaml
-
+import jinja2
+from joe.core.misc import h2, h3
 from joe.core.resources import Collector
-
 
 def worklet_entry(cijoe, args, step):
     """Produce a HTML report of the 'workflow.state' file in 'args.output'"""
@@ -13,15 +11,21 @@ def worklet_entry(cijoe, args, step):
     collector = Collector()
     collector.collect()
 
+    template_path = collector.resources["templates"]["report"]
+
+    print(f"template: {template_path}")
+    h3()
+
     # Load the workflow-state
     state_path = args.output / "workflow.state"
     with state_path.open() as state_file:
         state = yaml.load(state_file, Loader=yaml.SafeLoader)
 
     # Instantiate jinja2 template
-    template_resource = collector.resources["templates"]["core.report"]
+    template_resource = collector.resources["templates"]["report"]
     template = jinja2.Environment(
-        autoescape=True, loader=jinja2.FileSystemLoader(template_resource.path.parent)
+        autoescape=True,
+        loader=jinja2.FileSystemLoader(template_resource.path.parent)
     ).get_template(template_resource.path.name)
 
     # Render template using workflow-state as data and write it to file
