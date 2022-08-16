236081a143ea044fb8b06a07b258f9014d93fa98
Simon A. F. Lund <os@safl.dk>
2022-08-16 10:54:19 +0200

    ...
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

--- joe/src/joe/core/worklets/cmdrunner.py
+++ joe/src/joe/core/worklets/cmdrunner.py
@@ -1,10 +1,14 @@
+import errno
+
 
 def worklet_entry(args, collector, cijoe, step):
-    """Run commands listedin step['run']"""
+    """Run commands one at a time via cijoe.run()"""
 
     rcode = 0
+    if "with" not in step and "commands" not in step["with"]:
+        return errno.EINVAL
 
-    for cmd in step["run"]:
+    for cmd in step["with"]["commands"]:
         rcode, state = cijoe.run(cmd)
         if rcode:
             break
