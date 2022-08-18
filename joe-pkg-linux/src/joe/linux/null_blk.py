23ed19fe08684b8c48c6ca2d9d904ef5407bf10c
Simon A. F. Lund <os@safl.dk>
2022-08-18 21:44:29 +0200

    ...
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

--- joe-pkg-linux/src/joe/linux/null_blk.py
+++ joe-pkg-linux/src/joe/linux/null_blk.py
@@ -1,9 +1,18 @@
 """
-    null_blk module, providing helpers to load/unload null block instances
+    null_blk module, helpers to load/unload null block instances
 
     To use it, one must have permissions to modprobe, rmmod and modify the /syscfg
 
+    NOTE: This is **fully** re-targetable, that is, changing transport changes where it
+    is running.
+
     For reference, see: https://docs.kernel.org/block/null_blk.html
+
+    TODO
+    - Implement initialization of nullblk instances via /sys/kernel, this allows for
+      different device instances instead of N instances with the same configuration.
+      For example, one can instantiate a regular block-device as well as a zoned
+      block-device.
 """
 NULLBLK_MODULE_NAME = "null_blk"
 NULLBLK_SYSPATH = "/sys/kernel/config/nullb"
@@ -27,6 +36,8 @@ def insert(cijoe, config=None):
 def remove(cijoe):
     """Remove the null_blk kernel module"""
 
-    rcode, state = cijoe.run(f"rmdir {NULLBLK_SYSPATH}/nullb*")
+    # This can be used when instanttation via SYSPATH, however, commented out as it is
+    # not useful yet.
+    #cijoe.run(f"rmdir {NULLBLK_SYSPATH}/nullb*")
 
     return cijoe.run(f"modprobe -r {NULLBLK_MODULE_NAME}")
---
+++ joe-pkg-linux/src/joe/linux/worklets/nullblk_insert.py
@@ -0,0 +1,11 @@
+"""
+    Simple worklet collection Linux system information
+"""
+import joe.linux.null_blk as null_blk
+
+
+def worklet_entry(args, cijoe, step):
+    """Collect Linux system information"""
+
+
+    return rcode
