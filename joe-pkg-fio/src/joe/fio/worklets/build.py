441d65284020eaa11e486fa6f048ec99af93c9b5
Simon A. F. Lund <os@safl.dk>
2022-08-22 12:33:51 +0200

    ...
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

---
+++ joe-pkg-fio/src/joe/fio/worklets/build.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+    Installs qemu
+"""
+import errno
+from pathlib import Path
+
+
+def worklet_entry(args, cijoe, step):
+    """Install qemu"""
+
+    conf = cijoe.config.options.get("fio", None)
+    if not conf:
+        return errno.EINVAL
+
+    build_dir = Path(conf["repository"]["path"])
+
+    commands = [
+        "make clean",
+        "./configure",
+        "make",
+    ]
+    for cmd in commands:
+        rcode, _ = cijoe.run_local(cmd, cwd=build_dir)
+        if rcode:
+            return rcode
+
+    return rcode
