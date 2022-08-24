782ecd9baf646ae3b4d13fcfeb378a5da2815e06
Simon A. F. Lund <os@safl.dk>
2022-08-24 17:26:09 +0200

    ...
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

---
+++ joe-pkg-xnvme/src/joe/xnvme/worklets/install.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+    Build xNVMe
+"""
+import errno
+from pathlib import Path
+
+
+def worklet_entry(args, cijoe, step):
+    """Install qemu"""
+
+    conf = cijoe.config.options.get("xnvme", None)
+    if not conf:
+        return errno.EINVAL
+
+    repos_path = Path(conf["repository"]["path"])
+
+    commands = [
+        "make install",
+    ]
+    for cmd in commands:
+        rcode, _ = cijoe.run_local(cmd, cwd=repos_path)
+        if rcode:
+            return rcode
+
+    return rcode
