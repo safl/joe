7df87fb2a208a92ef074305e989c2764d5d73930
Simon A. F. Lund <os@safl.dk>
2022-08-28 23:17:05 +0200

    ...
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

--- joe-pkg-fio/src/joe/fio/wrapper.py
+++ joe-pkg-fio/src/joe/fio/wrapper.py
@@ -23,9 +23,18 @@ def fio(cijoe, args=""):
 
 
 def fio_script_engine(
-    cijoe, script, script_section, engine_label, device, be_opts, filename, output_path
+    cijoe,
+    script,
+    script_section,
+    engine_name,
+    device,
+    be_opts,
+    filename,
+    output_path,
+    extra_args,
 ):
     """
+    This
     Invoke 'fio' script with setup of io-engine
     """
 
@@ -36,14 +45,14 @@ def fio_script_engine(
     if engines is None:
         log.err("Configuration has no 'engines'")
         return errno.EINVAL, None
-    engine = cijoe.config.options.get(engine_label, None)
+    engine = cijoe.config.options.get(engine_name, None)
     if engine is None:
-        log.err(f"Configuration has no 'engine' with label({engine_label})")
+        log.err(f"Configuration has no 'engine' with label({engine_name})")
         return errno.EINVAL, None
 
-    # setup '--ioengine' options
+    # setup general '--ioengine' options
     if engine["type"] == "builitin":
-        args.append(f"--ioengine={engine_label}")
+        args.append(f"--ioengine={engine_name}")
     elif engine["type"] == "external_dynamic":
         args.append(f"--engine=external:{ engine['path'] }")
         args.append(f"--filename={filename}")
@@ -55,12 +64,29 @@ def fio_script_engine(
         return errno.EINVAL, None
 
     # setup 'xnvme' specific options
-    if engine_label == "xnvme":
+    if engine_name == "xnvme":
         args.append(f"--xnvme_async={ be_opts['async'] }")
         args.append(f"--xnvme_sync={ be_opts['sync'] }")
         args.append(f"--xnvme_admin={ be_opts['admin'] }")
         args.append(f"--xnvme_dev_nsid={ device['nsid'] }")
+        args.append(f"--filename={device['uri']}")
+    elif engine_name == "spdk":
+        args.append(
+            f"--filename=\"trtype=PCIe traddr={device['uri']} ns={device['nsid']}\""
+        )
+    elif engine_name == "spdk_bdev":
+        # generate spdk-conf
+        spdk_conf = []
+        spdk_conf.append("[Nvme]")
+        spdk_conf.append("  TransportID \"trtype:PCIe traddr:{device['uri']\" Nvme0")
+
+
+        args.append(f"--spdk_conf=/tmp/spdk.bdev.conf")
+        args.append(f"--filename=Nvme0n1")
         # TODO: add subnqn here
+
+    # Add extra arguments
+    args += extra_args
 
     if device:
         args.append(f"--filename={ device['uri'] }")
