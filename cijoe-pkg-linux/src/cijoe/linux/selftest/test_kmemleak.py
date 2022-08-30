161fec4f84af420b0823ad5ea9510d4f0648402c
Simon A. F. Lund <os@safl.dk>
2022-08-30 14:35:54 +0200

    ...
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

--- cijoe-pkg-linux/src/cijoe/linux/selftest/test_kmemleak.py
+++ cijoe-pkg-linux/src/cijoe/linux/selftest/test_kmemleak.py
@@ -3,34 +3,34 @@ import pytest
 import cijoe.linux.kmemleak as kmemleak
 
 
-def skip_config(cijoe):
+def skip_disabled(cijoe):
     """Skip testing when configuration is missing or module not enabled"""
 
     kmemleak = cijoe.config.options.get("kmemleak", None)
     if not kmemleak:
-        pytest.skip("missing: config['kmemleak'] configuration")
+        pytest.skip(reason="missing: config['kmemleak'] configuration")
 
     enabled = kmemleak.get("enabled", False)
     if not enabled:
-        pytest.skip("kmemleak is disabled: !config['kmemleak']['enabled']")
+        pytest.skip(reason="kmemleak is disabled: !config['kmemleak']['enabled']")
 
 
 def test_clear(cijoe):
-    skip_config(cijoe)
+    skip_disabled(cijoe)
 
     rcode, state = kmemleak.clear(cijoe)
     assert not rcode, "Failed clearing kmemleak"
 
 
 def test_scan(cijoe):
-    skip_config(cijoe)
+    skip_disabled(cijoe)
 
     rcode, state = kmemleak.scan(cijoe)
     assert not rcode, "Failed scanning kmemleak"
 
 
 def test_cat(cijoe):
-    skip_config(cijoe)
+    skip_disabled(cijoe)
 
     rcode, state = kmemleak.cat(cijoe)
     assert not rcode, "Failed cat'ing kmemleak"
--- cijoe-pkg-linux/src/cijoe/linux/selftest/test_null_blk.py
+++ cijoe-pkg-linux/src/cijoe/linux/selftest/test_null_blk.py
@@ -1,8 +1,20 @@
+import pytest
+
 import cijoe.linux.null_blk as null_blk
+
+
+def skip_when_config_has_no_remote(cijoe):
+    """Skip testing when configuration is module not enabled"""
+
+    transport = cijoe.config.options.get("transport", None)
+    if not transport:
+        pytest.skip(reason="skipping as there is no remote transport defined")
 
 
 def test_insert(cijoe):
     """Test the creation of null_block via module-load"""
+
+    skip_when_config_has_no_remote(cijoe)
 
     config = cijoe.config.options.get("null_blk", None)
     assert config, "Invalid environment configuration"
@@ -22,5 +34,8 @@ def test_insert(cijoe):
 
 
 def test_remove(cijoe):
+
+    skip_when_config_has_no_remote(cijoe)
+
     rcode, _ = null_blk.remove(cijoe)
     assert not rcode, "Failed removing kernel module"
