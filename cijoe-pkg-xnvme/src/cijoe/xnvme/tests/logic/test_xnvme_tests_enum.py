2c38947a1405c1148f6653b6bfbac47769431346
Simon A. F. Lund <os@safl.dk>
2022-08-31 11:34:14 +0200

    ...
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

--- cijoe-pkg-xnvme/src/cijoe/xnvme/tests/logic/test_xnvme_tests_enum.py
+++ cijoe-pkg-xnvme/src/cijoe/xnvme/tests/logic/test_xnvme_tests_enum.py
@@ -1,7 +1,29 @@
+"""
+This is a port of the tests:
+
+xnvme_tests_enum_any_be_multi.sh --> test_multi_all_be()
+xnvme_tests_enum_any_be_open.sh  --> test_open_all_be()
+xnvme_tests_enum_multi.sh        --> test_open()
+xnvme_tests_enum_open.sh         --> test_multi()
+"""
+from cijoe.xnvme.tests.conftest import xnvme_cli_args
 from cijoe.xnvme.tests.conftest import XnvmeDriver
+from cijoe.xnvme.tests.conftest import xnvme_device_driver as device
+from cijoe.xnvme.tests.conftest import xnvme_setup
 
 
-def test_open(cijoe):
+@pytest.mark.parametrize(
+    "device,be_opts",
+    xnvme_setup(labels=["dev"], opts=["be"]),
+    indirect=["device"],
+)
+def test_open(cijoe, device, be_opts):
+
+    rcode, _ = cijoe.run("xnvme_tests_enum open --count 4 --be {be_opts['be']}")
+    assert not rcode
+
+
+def test_open_all_be(cijoe):
 
     XnvmeDriver.kernel_attach(cijoe)
     rcode, _ = cijoe.run("xnvme_tests_enum open --count 4")
@@ -12,7 +34,18 @@ def test_open(cijoe):
     assert not rcode
 
 
-def test_multi(cijoe):
+@pytest.mark.parametrize(
+    "device,be_opts",
+    xnvme_setup(labels=["dev"], opts=["be"]),
+    indirect=["device"],
+)
+def test_multi(cijoe, device, be_opts):
+
+    rcode, _ = cijoe.run("xnvme_tests_enum multi --count 4 --be {be_opts['be']}")
+    assert not rcode
+
+
+def test_multi_all_be(cijoe):
 
     XnvmeDriver.kernel_attach(cijoe)
     rcode, _ = cijoe.run("xnvme_tests_enum multi --count 4")
@@ -21,3 +54,6 @@ def test_multi(cijoe):
     XnvmeDriver.kernel_detach(cijoe)
     rcode, _ = cijoe.run("xnvme_tests_enum multi --count 4")
     assert not rcode
+
+
+
