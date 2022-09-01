f015e02e60eaeded8f2431a499d6b553e3877a50
Simon A. F. Lund <os@safl.dk>
2022-09-01 19:22:21 +0200

    ...
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

--- cijoe-pkg-linux/setup.py
+++ cijoe-pkg-linux/setup.py
@@ -1,8 +1,14 @@
+"""
+This is a utility package, that is, contains worklets for building the kernel and
+wrappers around kernel features such as null_blk and kmemleak, since it is not tracking
+a specific version of the kernel, then the version number tracks the main cijoe package
+instead.
+"""
 from setuptools import find_namespace_packages, setup
 
 setup(
     name="cijoe-pkg-linux",
-    version="0.5.0.dev1",
+    version="0.9.0.dev1",
     author="Simon A. F. Lund",
     author_email="os@safl.dk",
     url="https://github.com/safl/joe-pkg-linux/",
